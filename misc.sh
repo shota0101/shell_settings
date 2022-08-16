03e63c1458bdc0994ebfb0590d7097407c3294a3
Shota Hayashi <shota.hayashi.0101@gmail.com>
2022-08-17 02:09:44 +0900

    Wed Aug 17 02:09:44 JST 2022

--- misc.sh
+++ misc.sh
@@ -3,8 +3,12 @@ PROMPT='%S%1~%s%(!.#.%%) '
 
 alias upsh="source ~/.zshrc" # シェルの設定を再読込
 alias up="cd .. ; pwd"
+alias o="open"
+alias d="open ."
 alias p="pbcopy"
 alias cpd="pwd | pbcopy"
+alias gre="find ./ -type f | xargs grepx"
+alias f="find . -name"
 alias cdate="date +%Y-%m-%d_%H-%M-%S | pbcopy"
 alias ctime="date '+%m-%d_' | pbcopy"
 alias cday="date '+%Y-%m-%d_' | pbcopy"
@@ -30,7 +34,6 @@ function tab-reset() {
     echo -ne "\033]6;1;bg;*;default\a"
 }
 
-alias d='tab-color 0 139 139' # darkcyan
 alias red='tab-color 204 51 51'
 alias green='tab-color 51 204 102'
 alias blue='tab-color 0 204 255'
@@ -58,29 +61,9 @@ function ad () {
 }
 
 # 分単位でsleepする
-function mov () {
-    echo ${1}
-    cp "${1}" ~/private/git/movie-controller/movies/current/movie1.mov
-    open -a /Applications/Firefox.app file:///Users/lin/private/git/movie-controller/page-mov1.html
-    # open -a /Applications/Google\ Chrome.app file:///Users/lin/private/git/movie-controller/page-mov1.html
-}
-
-# 分単位でsleepする
 function minutes () {
     seconds=`expr $1 \* 3600`
     sleep $seconds
-}
-
-function myfind() {
-    file_name=`ls -1R | grep -vE ":$" | peco`
-    current_directory=`pwd`
-    file_path=`find $current_directory -name $file_name`
-    echo $file_path
-    echo $file_path | pbcopy
-}
-
-function mygrep() {
-    grep -r $1 .
 }
 
 function mydiff() {
@@ -92,19 +75,9 @@ function mydiff() {
 alias m1="bash ~/private/git/movie-controller/next1.bash"
 alias m2="bash ~/private/git/movie-controller/next2.bash"
 alias m3="bash ~/private/git/movie-controller/next3.bash"
-alias a1="bash ~/private/git/movie-controller/audio1.bash"
-alias a2="bash ~/private/git/movie-controller/audio2.bash"
-
-# grep -r "keyword" directory
-alias gr="grep -ri" # -i → 検索条件に大文字と小文字の区別をなくす
-alias grab="grep -A 3 -B 3 -ri" # -A 3 -B 3 → 前後3行を表示
-alias grh="grep -rih" # -h → 検索結果の先頭にマッチしたファイル名を表示しない
-
-alias f="find . -name"
 
 alias chrome="open -a /Applications/Google\ Chrome.app"
 alias ij='open -b com.jetbrains.intellij'
-alias st='open -b com.torusknot.SourceTreeNotMAS'
 
 alias alarm_alerter="bash ~/private/git/shell_settings/util/time/alarm_alerter.bash"
 alias alarm_sound="bash ~/private/git/shell_settings/util/time/alarm_sound.bash"
@@ -138,14 +111,6 @@ function fa() {
     cd "${directory}"
     ad
     open "${directory}"
-}
-
-function wait() {
-    hour=`expr $1 \* 60 \* 60`
-    minute=`expr $2 \* 60`
-    allSecond=`expr $hour + $minute`
-    echo "${1}時間${2}分(${allSecond}秒)待機"
-    sleep $allSecond
 }
 
 alias sv="bash ~/private/git/box/program/shell_script/mac-air/search-videos.sh"
