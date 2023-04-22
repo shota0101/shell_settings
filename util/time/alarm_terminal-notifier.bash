8bf1648e6345daa213d54449fcbd9785e360c644
Shota Hayashi <shota.hayashi.0101@gmail.com>
2023-04-23 00:47:54 +0900

    alartコマンドで日付を跨ぐことを考慮

--- util/time/alarm_terminal-notifier.bash
+++ util/time/alarm_terminal-notifier.bash
@@ -1,17 +1,27 @@
 #!/bin/bash
 
+# miku-skin
+echo -ne "\033]6;1;bg;red;brightness;255\a"
+echo -ne "\033]6;1;bg;green;brightness;254\a"
+echo -ne "\033]6;1;bg;blue;brightness;236\a"
+
+function currentTime() {
+    date +"%Y%m%d%H%M"
+}
+
 time=$1
 message='alarm'
 if [ -n "$2" ]; then
     message='alarm : '$2
 fi
 
-# miku-skin
-echo -ne "\033]6;1;bg;red;brightness;255\a"
-echo -ne "\033]6;1;bg;green;brightness;254\a"
-echo -ne "\033]6;1;bg;blue;brightness;236\a"
+endTime=`date +"%Y%m%d"`${time}
+if [ `currentTime` -gt $endTime ]; then
+    # 日付を跨ぐことを考慮
+    endTime=`date -v+1d +"%Y%m%d"`${time}
+fi
 
-while [ `date +"%Y%m%d%H%M"` -lt `date +"%Y%m%d"`${time} ]
+while [ `currentTime` -lt $endTime ]
 do
     sleep 1
 done
