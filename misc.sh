8141f475378c6d4555abebd148bbe45eec10a73c
Shota Hayashi <shota.hayashi.0101@gmail.com>
2023-06-06 17:33:10 +0900

    通知のエイリアスを追加

--- misc.sh
+++ misc.sh
@@ -23,6 +23,7 @@ alias cdate="date +%Y-%m-%d_%H-%M-%S | pbcopy"
 alias ctime="date '+%m-%d_' | pbcopy"
 alias cday="date '+%Y-%m-%d_' | pbcopy"
 alias ds="find . -name '.DS_Store' -type f -ls -delete"
+alias n="terminal-notifier -title 'title' -message 'message' -sound Bottle"
 function mcd () { mkdir -p "$@" && eval cd "\"\$$#\""; }
 # lsの結果をクリップボードにコピー
 alias lc='ls -a | peco | pbcopy'
