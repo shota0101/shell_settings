03114fdc656ad3e3d91414a62e3357ed6e3df04b
Shota Hayashi <shota.hayashi.0101@gmail.com>
2023-10-14 22:42:01 +0900

    読み込みファイルの指定を現在のディレクトリの相対パスではなくホームディレクトリからの指定に変更

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
-. ./Documents/git/shell_settings/emacs.sh
-. ./Documents/git/shell_settings/misc.sh
-. ./Documents/git/shell_settings/git.sh
-. ./Documents/git/shell_settings/tmux.sh
+. ~/Documents/git/shell_settings/emacs.sh
+. ~/Documents/git/shell_settings/misc.sh
+. ~/Documents/git/shell_settings/git.sh
+. ~/Documents/git/shell_settings/tmux.sh
 # . ~/shell_settings/zsh.sh
 # source <(kubectl completion zsh)
 
-. ./Documents/git/shell_settings/env.sh
+. ~/Documents/git/shell_settings/env.sh
 
