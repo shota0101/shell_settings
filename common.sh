d083e618224e1b427ccf8f7f9a49e9e98eded8e2
Shota Hayashi <shota.hayashi.0101@gmail.com>
2023-10-23 13:58:18 +0900

    ubuntu brew

--- common.sh
+++ common.sh
@@ -4,12 +4,14 @@ if [ -d "/Volumes" ]; then
     GOOGLE_DRIVE_ROOT='/Volumes/GoogleDrive/My Drive'
     EMACS_DIRECTORY=$(brew --prefix emacs)
 elif [ -d "/mnt/c" ]; then
+    # windows wsl
     export PATH="$PATH:/home/linuxbrew/.linuxbrew/bin"
     GOOGLE_DRIVE_ROOT='/mnt/g/マイドライブ'
     EMACS_DIRECTORY='/usr'
     alias pbcopy='clip.exe'
 else
     # ubuntu
+    export PATH="$PATH:/home/linuxbrew/.linuxbrew/bin"
     GOOGLE_DRIVE_ROOT='/home/lin/g'
     EMACS_DIRECTORY='/usr'
     alias pbcopy='xsel --clipboard --input'
